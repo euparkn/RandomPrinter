aef9e0d3534985a51b0a1a75849cff3edfa57133
euparkn <euparkn@gmail.com>
2023-04-26 21:48:42 +0900

    feat: CircleButton (#2)

--- src/components/_Atoms/CircleButton.tsx
+++ src/components/_Atoms/CircleButton.tsx
@@ -1,5 +1,43 @@
-function CircleButton() {
-  return <button type="button">CircleButton</button>;
+import { ReactComponent as AddIcon } from "../../assets/icons/add.svg";
+import { ReactComponent as RemoveIcon } from "../../assets/icons/remove.svg";
+import { ReactComponent as CloseIcon } from "../../assets/icons/close.svg";
+
+type ButtonTypes = "add" | "remove" | "close";
+
+interface IProps {
+  buttonType: ButtonTypes;
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+const buttonIcon = (type: ButtonTypes) => {
+  switch (type) {
+    case "add":
+      return <AddIcon />;
+    case "remove":
+      return <RemoveIcon />;
+    case "close":
+      return <CloseIcon />;
+    default:
+      return undefined;
+  }
+};
+
+function CircleButton({
+  buttonType,
+  disabled = false,
+  onClick = undefined,
+}: IProps) {
+  return (
+    <button
+      className={`circle_button ${buttonType}`}
+      type="button"
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {buttonIcon(buttonType)}
+    </button>
+  );
 }
 
 export default CircleButton;
