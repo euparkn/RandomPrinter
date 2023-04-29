c7899c9fe6f0c8faf5b29c7510a0a7a03e30b3cc
euparkn <euparkn@gmail.com>
2023-04-29 20:59:13 +0900

    feat: ToggleSwitch (#4)

--- src/components/_Atoms/ToggleSwitch.tsx
+++ src/components/_Atoms/ToggleSwitch.tsx
@@ -1,5 +1,18 @@
-function ToggleSwitch() {
-  return <button type="button">ToggleSwitch</button>;
+import Text from "./Text";
+
+interface IProps {
+  label?: string;
+  value?: boolean;
+  onChange?: () => void;
+}
+function ToggleSwitch({ label, value = false, onChange }: IProps) {
+  return (
+    <label className="toggle_switch">
+      {label && <Text text={label} />}
+      <input type="checkbox" defaultChecked={value} onChange={onChange} />
+      <span />
+    </label>
+  );
 }
 
 export default ToggleSwitch;
