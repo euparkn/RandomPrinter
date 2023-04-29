7efd03772c990d34fffc0b6eca9b652e1d13d715
euparkn <euparkn@gmail.com>
2023-04-29 21:34:49 +0900

    feat: TextButton and add skip.svg (#4)

--- src/components/_Atoms/TextButton.tsx
+++ src/components/_Atoms/TextButton.tsx
@@ -1,5 +1,36 @@
-function TextButton() {
-  return <button type="button">TextButton</button>;
+import { ReactComponent as SkipIcon } from "../../assets/icons/skip.svg";
+import { ReactComponent as AddIcon } from "../../assets/icons/add.svg";
+
+type TextIconTypes = "add" | "skip" | undefined;
+
+interface IProps {
+  text?: string;
+  IconType?: TextIconTypes;
+  onClick?: () => void;
+}
+
+const buttonIcon = (type: TextIconTypes) => {
+  switch (type) {
+    case "add":
+      return <AddIcon />;
+    case "skip":
+      return <SkipIcon />;
+    default:
+      return undefined;
+  }
+};
+
+function TextButton({ text, IconType, onClick }: IProps) {
+  return (
+    <button
+      className={`text-button ${IconType}`}
+      type="button"
+      onClick={onClick}
+    >
+      {buttonIcon(IconType)}
+      {text}
+    </button>
+  );
 }
 
 export default TextButton;
