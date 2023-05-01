7d0831366c279a64a53de7cfb311f9583c22629d
euparkn <euparkn@gmail.com>
2023-05-01 13:42:44 +0900

    feat: listStateAtom with recoil (#8)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RecoilRoot } from "recoil";
 import reportWebVitals from "./reportWebVitals";
 import App from "./pages/app/App";
 import "./assets/styles/index.scss";
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>
 );
 
