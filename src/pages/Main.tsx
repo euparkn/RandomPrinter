2196df345ac98d105950233e2fc26c2b56974fc9
euparkn <euparkn@gmail.com>
2023-05-13 14:37:50 +0900

    style: main layout (#1)

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,10 @@
 import Aside from "../components/Aside";
+import Content from "../components/Content";
 
 function Main() {
   return (
     <div className="main">
+      <Content />
       <Aside />
     </div>
   );
