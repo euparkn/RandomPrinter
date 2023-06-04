b1d1acb205fc6e1743c70f47d1837cf226be2321
euparkn <euparkn@gmail.com>
2023-06-04 14:02:46 +0900

    feat: AsideListAtom default (#10)

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,23 @@ import { IAsideListItem } from "../types/listItemTypes";
 
 export const asideListAtom = atom<IAsideListItem[]>({
   key: "asideList",
-  default: [],
+  default: [
+    {
+      id: 1,
+      text: "text 1",
+      count: 1,
+    },
+    {
+      id: 2,
+      text: "text 2",
+      count: 1,
+    },
+    {
+      id: 3,
+      text: "text 3",
+      count: 1,
+    },
+  ],
 });
 
 export const asideOptionAtom = atom({
