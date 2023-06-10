82e2b67e656bbe777e5996e812909bf4441129bd
euparkn <euparkn@gmail.com>
2023-06-10 20:28:52 +0900

    feat: Result (#16)

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -1,5 +1,18 @@
+import { useRecoilValue } from "recoil";
+import { printerListAtom } from "../../store";
+import ListItem from "../_Molecules/ListItem";
+
 function Result() {
-  return <div className="result">result</div>;
+  const printerList = useRecoilValue(printerListAtom);
+  return (
+    <div className="result">
+      <div className="result-list">
+        {printerList.map((item, i) => (
+          <ListItem key={Math.random()} num={i + 1} text={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Result;
