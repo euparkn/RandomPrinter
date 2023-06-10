c66b771d2fb847318e8c231c18a9ec9ef2a5f143
euparkn <euparkn@gmail.com>
2023-06-10 20:28:15 +0900

    feat: ListItem (#16)

---
+++ src/components/_Molecules/ListItem.tsx
@@ -0,0 +1,12 @@
+import ListStyleCircle from "../_Atoms/ListStyleCircle";
+
+function ListItem({ num, text }: { num: number; text: string }) {
+  return (
+    <div className="list-item">
+      <ListStyleCircle num={num} />
+      <p>{text}</p>
+    </div>
+  );
+}
+
+export default ListItem;
