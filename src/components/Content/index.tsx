bcd0a409be896d458f9a05921df996fadae491af
euparkn <euparkn@gmail.com>
2023-06-12 21:43:35 +0900

    style: skip button (#16)

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,14 +1,33 @@
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+
+import { ReactComponent as SkipIcon } from "../../assets/icons/skip.svg";
+
+import { printerListAtom } from "../../store";
+
 import Printer from "../Printer";
 import AsideToggleButton from "../_Atoms/AsideTogglebutton";
+import TextButton from "../_Atoms/TextButton";
 
 function Content() {
+  const navigate = useNavigate();
+
+  const printerList = useRecoilValue(printerListAtom);
+
   return (
     <div className="content">
       <div className="content-section">
         <Printer />
         <AsideToggleButton />
-        <NavLink to="/result">result</NavLink>
+        {printerList.length !== 0 && (
+          <TextButton
+            text="Skip"
+            onClick={() => navigate("/result")}
+            backgroundColor="transparent"
+            color="#aaa"
+            icon={<SkipIcon fill="#aaa" />}
+          />
+        )}
       </div>
     </div>
   );
