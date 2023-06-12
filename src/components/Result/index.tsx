b99c653f93eab8814dbbaea9951275ec21431f16
euparkn <euparkn@gmail.com>
2023-06-12 21:44:26 +0900

    feat: Restart button (#16)

--- src/components/Result/index.tsx
+++ src/components/Result/index.tsx
@@ -1,8 +1,11 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
+
 import { printerListAtom } from "../../store";
+
 import ListItem from "../_Molecules/ListItem";
+import TextButton from "../_Atoms/TextButton";
 
 function Result() {
   const navigate = useNavigate();
@@ -16,10 +19,19 @@ function Result() {
 
   return (
     <div className="result">
+      <h1>Results</h1>
       <div className="result-list">
         {printerList.map((item, i) => (
           <ListItem key={Math.random()} num={i + 1} text={item} />
         ))}
+      </div>
+      <div className="result-btn">
+        <TextButton
+          text="Restart"
+          onClick={() => navigate(-1)}
+          color="#5499c7"
+        />
+        <TextButton text="Copy" backgroundColor="#5499c7" color="#f8f8f8" />
       </div>
     </div>
   );
