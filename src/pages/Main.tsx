71b09b54f40f2ea9505aff5295029a06acfef533
euparkn <euparkn@gmail.com>
2023-06-14 23:30:03 +0900

    fix: move AsideTogleButton

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,13 @@
 import Aside from "../components/Aside";
 import Content from "../components/Content";
+import AsideToggleButton from "../components/_Atoms/AsideTogglebutton";
 
 function Main() {
   return (
     <div className="main">
       <Content />
       <Aside />
+      <AsideToggleButton />
     </div>
   );
 }
