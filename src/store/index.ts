8073e4ed6327ecbd8bce702c9488b5cacd395491
euparkn <euparkn@gmail.com>
2023-06-18 16:04:50 +0900

    feat: use localStorage to save and load (#19)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,30 +1,57 @@
 import { atom } from "recoil";
 import { IAsideListItem } from "../types/listItemTypes";
+import { IOptions } from "../types/OptionsType";
+
+const defaultAsideList = [
+  {
+    id: 1,
+    text: "text 1",
+    count: 1,
+  },
+  {
+    id: 2,
+    text: "text 2",
+    count: 1,
+  },
+  {
+    id: 3,
+    text: "text 3",
+    count: 1,
+  },
+];
 
 export const asideListAtom = atom<IAsideListItem[]>({
   key: "asideList",
-  default: [
-    {
-      id: 1,
-      text: "text 1",
-      count: 1,
-    },
-    {
-      id: 2,
-      text: "text 2",
-      count: 1,
-    },
-    {
-      id: 3,
-      text: "text 3",
-      count: 1,
+  default: [],
+  effects: [
+    ({ setSelf }) => {
+      const savedItem = localStorage.getItem("random-printer-list");
+      if (savedItem != null) {
+        setSelf(JSON.parse(savedItem));
+      } else {
+        setSelf(defaultAsideList);
+      }
     },
   ],
 });
 
-export const optionsAtom = atom({
+export const optionsAtom = atom<IOptions>({
   key: "options",
   default: { animation: true, darkMode: false },
+  effects: [
+    ({ setSelf, onSet }) => {
+      const savedItem = localStorage.getItem("random-printer-options");
+      if (savedItem != null) {
+        setSelf(JSON.parse(savedItem));
+      }
+      onSet((newValue: IOptions) => {
+        localStorage.setItem(
+          "random-printer-options",
+          JSON.stringify(newValue)
+        );
+      });
+    },
+  ],
 });
 
 export const printerListAtom = atom<string[]>({
